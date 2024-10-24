5fd60c6ced1a785fe40bfd2deca01ba3f7493025
krutikgabani <krutikgabani1@gmail.com>
2024-10-24 11:07:49 +0530

    krutik

--- Assignment 6/web3/BuyMeACoffee Comtracts/test/Lock.js
+++ Assignment 6/web3/BuyMeACoffee Comtracts/test/Lock.js
@@ -13,6 +13,11 @@ describe("Lock", function () {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
 
+
+
+
+    
+
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
